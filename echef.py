cc6b43bcd57e2a17fa87705a3724c3e641d6051b
Sean Kau <sean@kautown.com>
2013-01-22 14:52:18 -0800

    It works now

--- echef.py
+++ echef.py
@@ -1,10 +1,16 @@
 import xml.etree.ElementTree as ET
 import cheferizer as EC 
 import re
+import sys
+import os
 
 ET.register_namespace('', "urn:oasis:names:tc:xliff:document:1.2")
 
-tree = ET.parse("sample_sv-US-zyyy.xlf")
+if len(sys.argv) != 2:
+	sys.exit("Please provide the name of the file")
+
+fileName = sys.argv[1]
+tree = ET.parse(fileName)
 root = tree.getroot()
 
 nm = re.search("{.*}", root.tag)
@@ -14,8 +20,11 @@ for	neighbor in root.iter(namespace + 'trans-unit') :
 	e = EC.EChef()
 	source = neighbor.findall(namespace + 'source')[0]
 	target = neighbor.findall(namespace + 'target')[0]
+	neighbor.set("approved", "yes")
 	neighbor.remove(target)
 	target.text = e.parse(source.text)
 	neighbor.append(target)
 
-tree.write('output.xml')
+fName = os.path.splitext(fileName)[0]
+
+tree.write(fName + "TR" + '.xlf')
